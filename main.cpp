59225f8d691e85cf7ce016a3162e6ac7d10d4b86
thesecondpioneer <92123473+thesecondpioneer@users.noreply.github.com>
2021-10-08 00:02:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int a,b,c;
+    double a,b,c;
     cin >> a >> b >> c;
     cout << ((-b+sqrt(b*b-4*a*c))/(2*a)) << '\n' << ((-b-sqrt(b*b-4*a*c))/(2*a));
     return 0;
