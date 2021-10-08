3c484f7947fc8ab870b4746dd59eb294d1648ef7
pio <92123473+thesecondpioneer@users.noreply.github.com>
2021-10-08 21:08:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 int main() {
     double a,b,c;
     cin >> a >> b >> c;
-    cout << ((-b+sqrt(b*b-4*a*c))/(2*a)) << '\n' << ((-b-sqrt(b*b-4*a*c))/(2*a));
+    if ((a >= pow(10,153)) or (b >= pow(10,153)) or (c >= pow(10,153))){
+        cout << "One or several numbers are too big";
+    }
+    else
+    if ((abs(a) <= pow(10,-153) and (a!=0)) or (abs(b) <= pow(10,-153)and (b!=0)) or (abs(c) <= pow(10,-153)and (c!=0))){
+        cout << "One or several numbers are too small";
+    }
+    else if (a!=0) {
+        cout << ((-b + sqrt(b * b - 4 * a * c)) / (2 * a)) << '\n' << ((-b - sqrt(b * b - 4 * a * c)) / (2 * a));
+    }
+    else{
+        if ((b == 0) and (c==0)){
+            cout << "Any number";
+        }
+        else{
+            if (b == 0){
+                cout << "False";
+            }
+            else cout << -c/b;
+        }
+    }
     return 0;
 }
